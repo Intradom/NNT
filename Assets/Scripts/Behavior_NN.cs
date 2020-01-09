47af58a7952122b8fb5539915aa628b2716978ef
Bo <zboyang@umich.edu>
2020-01-09 14:54:31 -0500

    Reformatted weight structure

--- Assets/Scripts/Behavior_NN.cs
+++ Assets/Scripts/Behavior_NN.cs
@@ -4,55 +4,108 @@ using UnityEngine;
 
 public class Behavior_NN : MonoBehaviour
 {
+    private struct NN_Node
+    {
+        public List<float> weights;
+        public float bias;
+    }
+
     [SerializeField] private int num_hidden_layers = 0;
     [SerializeField] private int hidden_layer_size = 0;
 
     private static Behavior_NN self_NN; // Singleton
-    private List<List<float>> NN_data = new List<List<float>>();
+    private List<List<NN_Node>> NN_weights = new List<List<NN_Node>>();
+    private int input_size = 0;
+    private int output_size = 0;
     private bool proper = false;
 
     public bool Init(int input_layer_size, int output_layer_size)
     {
         proper = (input_layer_size > 0) && (output_layer_size > 0);
+        input_size = input_layer_size;
+        output_size = output_layer_size;
 
-        NN_data.Clear();
+        NN_weights.Clear();
 
-        NN_data.Add(RandomList(input_layer_size));
-        for (int i = 0; i < num_hidden_layers; ++i)
+        // Input layer to hidden layer
+        List<NN_Node> i2h_layer_list = new List<NN_Node>();
+        for (int i = 0; i < hidden_layer_size; ++i)
         {
-            NN_data.Add(RandomList(hidden_layer_size));
+            i2h_layer_list.Add(RandomNode(input_layer_size));
         }
-        NN_data.Add(RandomList(output_layer_size));
+        NN_weights.Add(i2h_layer_list);
 
-        //DebugWeights();
+        // Hidden layer to hidden layer
+        for (int i = 0; i < num_hidden_layers - 1; ++i) // Only necessary if h_layers > 1
+        {
+            List<NN_Node> h2h_layer_list = new List<NN_Node>();
+            for (int j = 0; j < hidden_layer_size; ++j)
+            {
+                h2h_layer_list.Add(RandomNode(hidden_layer_size));
+            }
+            NN_weights.Add(h2h_layer_list);
+        }
 
+        // Hidden layer to output layer
+        List<NN_Node> h2o_layer_list = new List<NN_Node>();
+        for (int i = 0; i < output_layer_size; ++i)
+        {
+            h2o_layer_list.Add(RandomNode(hidden_layer_size));
+        }
+        NN_weights.Add(h2o_layer_list);
+
+        DebugWeights();
         return proper;
+    }
+
+    public bool FF_Pass(ref float[] input, ref float[] output)
+    {
+        // Make sure the input/output matches the NN dimensions
+        if (input.Length != input_size || output.Length != output_size)
+        {
+            Debug.Log("FF_Pass input/output size mismatch");
+            return false;
+        }
+
+
+
+        return true;
     }
 
     private void DebugWeights()
     {
-        // Prints all weights in NN_data
+        // Prints all weights in NN_weights
         Debug.Log("Weights: ");
-        for (int i = 0; i < NN_data.Count; ++i)
+        for (int i = 0; i < NN_weights.Count; ++i)
         {
-            string debug_msg = "[ ";
-            for (int j = 0; j < NN_data[i].Count; ++j)
+            string debug_msg = "";
+            for (int j = 0; j < NN_weights[i].Count; ++j)
             {
-                debug_msg += NN_data[i][j] + " ";
+                // Each node
+                debug_msg += "[ ";
+                for (int k = 0; k < NN_weights[i][j].weights.Count; ++k)
+                {
+                    debug_msg += NN_weights[i][j].weights[k] + " ";
+                }
+                debug_msg += "; " + NN_weights[i][j].bias + " ]";
             }
-            Debug.Log(debug_msg + "]");
+            Debug.Log(debug_msg);
         }
     }
 
-    private List<float> RandomList(int size)
+    private NN_Node RandomNode(int size)
     {
+        NN_Node node = new NN_Node();
+
         List<float> rlist = new List<float>();
         for (int i = 0; i < size; ++i)
         {
             rlist.Add(Random.value);
         }
+        node.weights = rlist;
+        node.bias = Random.value;
 
-        return rlist;
+        return node;
     }
 
     private void Awake()
